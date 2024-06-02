38e241b199998632022acbd1f121b8aa8408a996
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-06-02 21:51:03 +0530

    redesiging of form,remove header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Header from "./components/Header";
 import Contact from "./components/Contact";
 import Hello from "./components/Hello";
 
 function App() {
   return (
     <>
-      <Header />
       <Contact />
     </>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import "../App.css";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <form className="max-w-lg mx-auto mt-5 p-4 rounded-xl shadow-xl shadow-purple-500/50 ">
+      <div className=" mt-5 mb-5 rounded-lg h-32 flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-5 text-2xl text-white">
+        <h1>
+          <i className="fa-solid fa-id-card-clip text-xl"></i> Contact Manager
+        </h1>
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="username"
+          className="block  text-sm font-medium text-white"
+        >
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="username"
+          className="block w-full p-2.5 bg-transparent text-slate-400  border-2 border-t-transparent border-r-transparent border-l-transparent border-b-indigo-500 transition-all focus:bg-purple-900 focus:text-white"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="useremail"
+          className="block  text-sm font-medium text-white"
+        >
+          E-mail
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="useremail"
+          className=" block w-full p-2.5 bg-transparent text-slate-400 border-2 border-t-transparent border-r-transparent border-l-transparent border-b-indigo-500 transition-all focus:bg-purple-900 focus:text-white"
+        />
+      </div>
+      <div>
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            +Add contact
+          </span>
+        </button>
+      </div>
+    </form>
   );
 };
 
