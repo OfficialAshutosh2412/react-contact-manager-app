0db2eb05f72a9844f22363a34be1c7da8c62a286
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-06-04 23:10:47 +0530

    redesigned form and inserted data rendered.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,53 +25,55 @@ function Contact({ contactProp }) {
   };
 
   return (
-    <form className="max-w-lg mx-auto mt-5 p-4 rounded-xl shadow-xl shadow-purple-500/50 ">
+    <form className="w-4/5 mx-auto mt-5 p-4 rounded-xl shadow-xl shadow-purple-500/50 ">
       <div className=" mt-5 mb-5 rounded-lg h-32 flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-5 text-2xl text-white">
         <h1>
           <i className="fa-solid fa-id-card-clip text-xl"></i> Contact Manager
         </h1>
       </div>
-      <div className="mb-5">
-        <label
-          htmlFor="username"
-          className="block  text-sm font-medium text-white"
-        >
-          Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="username"
-          value={contactInfo.name}
-          onChange={handleChange}
-          className="block w-full p-2.5 bg-transparent text-slate-400  border-2 border-t-transparent border-r-transparent border-l-transparent border-b-indigo-500 transition-all focus:bg-purple-900 focus:text-white"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="useremail"
-          className="block  text-sm font-medium text-white"
-        >
-          E-mail
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="useremail"
-          value={contactInfo.email}
-          onChange={handleChange}
-          className=" block w-full p-2.5 bg-transparent text-slate-400 border-2 border-t-transparent border-r-transparent border-l-transparent border-b-indigo-500 transition-all focus:bg-purple-900 focus:text-white"
-        />
-      </div>
-      <div>
-        <button
-          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-          onClick={handleSubmit}
-        >
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            +Add contact
-          </span>
-        </button>
+      <div className="flex w-5/5 gap-5">
+        <div className="mb-5 w-full">
+          <label
+            htmlFor="username"
+            className="block  text-sm font-medium text-white"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="username"
+            value={contactInfo.name}
+            onChange={handleChange}
+            className="block w-full p-2.5 bg-purple-500 text-white  border-2 border-t-transparent border-r-transparent border-l-transparent border-b-indigo-500 transition-all focus:bg-purple-900 focus:text-white rounded"
+          />
+        </div>
+        <div className="mb-5 w-full">
+          <label
+            htmlFor="useremail"
+            className="block  text-sm font-medium text-white"
+          >
+            E-mail
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="useremail"
+            value={contactInfo.email}
+            onChange={handleChange}
+            className="block w-full p-2.5 bg-purple-500 text-white  border-2 border-t-transparent border-r-transparent border-l-transparent border-b-indigo-500 transition-all focus:bg-purple-900 focus:text-white rounded"
+          />
+        </div>
+        <div>
+          <button
+            className="w-full mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+            onClick={handleSubmit}
+          >
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              +Add contact
+            </span>
+          </button>
+        </div>
       </div>
     </form>
   );
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const ContactList = ({ renderContacts }) => {
   console.log(renderContacts);
+  const contactData = renderContacts.map((_data) => {
+    return (
+      <div>
+        <div>{_data.name}</div>
+        <div>{_data.email}</div>
+      </div>
+    );
+  });
   return (
     <div>
       <h1>hi list</h1>
+
+      <div>{contactData}</div>
     </div>
   );
 };
