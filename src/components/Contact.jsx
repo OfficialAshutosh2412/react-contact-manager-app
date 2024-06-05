13713f9e6dc98e122c6bb665b013607cf5cb00b3
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-06-05 23:58:39 +0530

    output boxes designed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../App.css";
+import "../index.css";
 
 function Contact({ contactProp }) {
   const [contactInfo, setContactInfo] = useState({
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import "../index.css";
 
 const ContactList = ({ renderContacts }) => {
   console.log(renderContacts);
   const contactData = renderContacts.map((_data) => {
     return (
-      <div>
-        <div>{_data.name}</div>
+      <div className="flex flex-1 flex-col gap-4 justify-center items-center rounded-xl bg-gradient-to-r from-purple-950 via-purple to-purple-600  h-40 text-white p-4 relative ">
+        <div className="text-3xl">{_data.name}</div>
         <div>{_data.email}</div>
+        <span className="absolute top-0  cross-btn bg-rose-500 rounded-full flex justify-content-center items-center h-10 p-3 cursor-pointer">
+          <button>
+            <i class="fa-solid fa-trash-can"></i>
+          </button>
+        </span>
       </div>
     );
   });
   return (
     <div>
-      <h1>hi list</h1>
-
-      <div>{contactData}</div>
+      <div className="w-4/5 mx-auto mt-5">
+        <div className="flex flex-wrap gap-10">{contactData}</div>
+      </div>
     </div>
   );
 };
