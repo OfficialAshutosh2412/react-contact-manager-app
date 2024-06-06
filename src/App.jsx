6706357de91c643649809c3b5f2a645b05973729
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-06-06 22:38:26 +0530

    data deletion functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
 import Contact from "./components/Contact";
 import ContactList from "./components/ContactList";
+import uuid4 from "uuid4";
 function App() {
   const [renderContacts, setRenderContacts] = useState([]);
   const contactProp = (data) => {
-    setRenderContacts([...renderContacts, data]);
+    setRenderContacts([...renderContacts, { id: uuid4(), data }]);
+  };
+
+  const removeContact = (id) => {
+    const newlist = renderContacts.filter((val) => {
+      return val.id !== id;
+    });
+    setRenderContacts(newlist);
   };
 
   return (
     <>
       <Contact contactProp={contactProp} />
-      <ContactList renderContacts={renderContacts} />
+      <ContactList
+        renderContacts={renderContacts}
+        removeContact={removeContact}
+      />
     </>
   );
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "../index.css";
 
-const ContactList = ({ renderContacts }) => {
+const ContactList = (props) => {
+  const { renderContacts, removeContact } = props;
   console.log(renderContacts);
-  const contactData = renderContacts.map((_data) => {
+  const contactData = renderContacts.map((val) => {
     return (
       <div className="flex flex-1 flex-col gap-4 justify-center items-center rounded-xl bg-gradient-to-r from-purple-950 via-purple to-purple-600  h-40 text-white p-4 relative ">
-        <div className="text-3xl">{_data.name}</div>
-        <div>{_data.email}</div>
+        <div className="text-3xl">{val.data.name}</div>
+        <div>{val.data.email}</div>
         <span className="absolute top-0  cross-btn bg-rose-500 rounded-full flex justify-content-center items-center h-10 p-3 cursor-pointer">
-          <button>
+          <button onClick={() => removeContact(val.id)}>
             <i class="fa-solid fa-trash-can"></i>
           </button>
         </span>
